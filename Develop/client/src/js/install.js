fd00b1496ea59573e7eb5204f9f18a5cac991251
Beurre-moutarde <christopher.m.lebreton@gmail.com>
2023-04-15 14:45:57 +0200

    install.js updated

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -3,22 +3,29 @@ const butInstall = document.getElementById("buttonInstall");
 // Logic for installing the PWA
 window.addEventListener("beforeinstallprompt", (event) => {
   // Save the triggered events
-  window.delayedPrompt = event;
+  window.deferredPrompt = event;
+
+  // Remove the hidden class from the button.
   butInstall.classList.toggle("hidden", false);
 });
 
 butInstall.addEventListener("click", async () => {
-  const promptEvent = delayedPrompt;
+  const promptEvent = deferredPrompt;
 
   if (!promptEvent) {
     return;
   }
+
+  //Show prompt
   promptEvent = prompt();
-  window.delayedPrompt = null;
+
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
 
   butInstall.classList.toggle("hidden", true);
 });
 
 window.addEventListener("appinstalled", (event) => {
-  window.delayedPrompt = null;
+  // Clear prompt
+  window.deferredPrompt = null;
 });
